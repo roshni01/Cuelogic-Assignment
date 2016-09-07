2ff6872c2f9f638271cbb5e310cca47c96b523d0
Rajesh Darak <rajesh.darak>
2016-09-07 15:23:59 +0530

    Initial Commit

---
+++ Cuelogic-Assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cuelogic-Assignment
+//
+//  Created by Rajesh Darak on 07/09/16.
+//  Copyright © 2016 Roshani Mahajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
